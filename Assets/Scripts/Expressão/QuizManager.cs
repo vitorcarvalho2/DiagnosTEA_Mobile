60e3e509ef51fe6c88c997daf9d531dd7953d6ff
VitorCarvalho2 <vitorco.oliveira@gmail.com>
2024-09-24 03:09:27 -0300

    " aplicação total do jogo de expressões para uso e retorno a cena principal"

--- Assets/Scripts/Expressão/QuizManager.cs
+++ Assets/Scripts/Expressão/QuizManager.cs
@@ -1,63 +1,49 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using System.Collections;
+
 
 public class QuizManager : MonoBehaviour
 {
     public List<QuestionsandAnwers> QnA;//lista de perguntas
     public GameObject[] options; // array de opções de resposta
-    public TextMeshProUGUI QuestionTxt; // texto da pergunta
-    public TextMeshProUGUI ScoreTxt; // texto de pontuação
-    public TextMeshProUGUI ScoreTxt2; // texto de pontuação
-    public GameObject QuizPanel; // painel principal
-    public GameObject GoPanel; // painel final
-    public GameObject EndPanel; // painel final
-    public int CurrentQuestion;
-    int TotalQuestion = 0;
-    public int Score = 0;
+    public Text QuestionTxt; // texto da pergunta
+    public Text ScoreTxt; // texto de pontuação
+    public GameObject QuizPanel,EndPanel; // painel principal e painel final
+    public int CurrentQuestion; // pergunta atual
+    int TotalQuestion = 0; // quantidade de perguntas
+    public int Score = 0; // pontuação
+
+    [SerializeField]
+    public SceneInfo sceneInfo;
+
+    public Button voltaJogo;
 
     private void Start()//inicia o jogo deixando o painel principal visivel e os outros nao
     {
-        QuizPanel.SetActive(true);
-        GoPanel.SetActive(false);
-        EndPanel.SetActive(false);
+        voltaJogo.onClick = new Button.ButtonClickedEvent();
+        voltaJogo.onClick.AddListener(() => VoltaJogo());
         TotalQuestion = QnA.Count; // coloca a quantidade de perguntas
         GenerateQuestion(); // gera a primeira pergunta aleatoria
     }
 
-    
-
-    void GameOver()
-    { 
+    void FimDeJogo()
+    {   
         QuizPanel.SetActive(false);
-        GoPanel.SetActive(true);
-        ScoreTxt.text = $"{Score}/{TotalQuestion}";
-        
-        Debug.Log("sem mais perguntas"+ Time.time);
-    }
-    public void Retry()
-    {
-        
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        //adicionar tempo padronizado
+        StartCoroutine(PainelFimDeJogo());
     }
 
-    public void End()
-    {
-        GoPanel.SetActive(false);
-        EndPanel.SetActive(true);
-        ScoreTxt2.text = $"{Score}/{TotalQuestion}";
-    }
+
     public void Correct()
     {
         Score += 1;
         QnA.RemoveAt(CurrentQuestion);
         GenerateQuestion();
         AnswersScript answer = options[CurrentQuestion].GetComponent<AnswersScript>();
-        
-
-
     }
 
     public void Wrong()
@@ -65,8 +51,6 @@ public class QuizManager : MonoBehaviour
         QnA.RemoveAt(CurrentQuestion);
         GenerateQuestion();
         AnswersScript answer = options[CurrentQuestion].GetComponent<AnswersScript>();
-        
-
     }
 
     void SetAnswers()
@@ -79,7 +63,6 @@ public class QuizManager : MonoBehaviour
             if (QnA[CurrentQuestion].CorrectAnswer == i + 1)
             {
                 options[i].GetComponent<AnswersScript>().IsCorrect = true;
-
             }
         }
     }
@@ -96,10 +79,19 @@ public class QuizManager : MonoBehaviour
         }
         else
         {
-            GameOver();
+            FimDeJogo();
         }
     }
 
+    void VoltaJogo()
+    {
+        SceneManager.LoadScene("fase01");
+    }
 
-
+    private IEnumerator PainelFimDeJogo()
+    {
+        yield return new WaitForSeconds(1.5f);
+        ScoreTxt.text = $"Você acertou {Score}/{TotalQuestion} perguntas!";
+        EndPanel.SetActive(true);
+    }
 }
