a1c50b872217428988d863bcb73a6f18c295f7e9
VitorCarvalho2 <vitorco.oliveira@gmail.com>
2024-09-28 03:31:14 -0300

    mais ajustes

---
+++ Assets/Scripts/Fase01/EndFase01.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndFase01 : MonoBehaviour
+{
+    [SerializeField]
+    SceneInfo sceneInfo;
+
+    void Start()
+    {
+        if (sceneInfo.destroyedObjects.Count == 3)
+        {
+            StartCoroutine(EndFase());
+        }
+    }
+
+    IEnumerator EndFase()
+    {
+        yield return new WaitForSeconds(0.1f);
+        SceneManager.LoadScene("Fim");        
+    }
+}
+
--- Assets/Scripts/Select Player/ChooseChar.cs
+++ Assets/Scripts/Select Player/ChooseChar.cs
@@ -13,13 +13,13 @@ public class ChooseChar : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(DelayedExecution());
+        StartCoroutine(SetPlayer());
         
     }
 
-    IEnumerator DelayedExecution()
+    IEnumerator SetPlayer()
     {
-        yield return new WaitForSeconds(0.2f); // Aguarda 2 segundos
+        yield return new WaitForSeconds(0.2f); // Aguarda 0.2 segundos para verificar os objetos destruidos
 
         // Cria o objeto
         prefabName = "Personagens/" + PlayerPrefs.GetString("charName").Split("(Clone)")[0];
