8304b1d6fea9820de77ff7e20fd622a3e527078c
VitorCarvalho2 <vitorco.oliveira@gmail.com>
2024-11-01 19:02:35 -0300

    adição tempo personagens

--- Assets/Scripts/Dados/SceneInfo.cs
+++ Assets/Scripts/Dados/SceneInfo.cs
@@ -5,7 +5,7 @@ using System;
 [CreateAssetMenu(fileName = "SceneInfo", menuName = "Persistence")]
 public class SceneInfo : ScriptableObject
 {
-    //this class contains the information about player curent position and a list of destroyed objects, it has to be instanciated in every itembox
+    //this class contains  the information about player curent position and a list of destroyed objects, it has to be instanciated in every itembox
     //to collect position information and to add the object to the destoyed list. It also has to be added in each minigame end script like "checarFimQC"
     public Vector3 position;
     public string playerName;
