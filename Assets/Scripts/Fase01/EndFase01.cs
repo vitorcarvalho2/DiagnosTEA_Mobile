967029f0719570bb693927b6562908686e7aeed7
VitorCarvalho2 <vitorco.oliveira@gmail.com>
2024-11-16 03:40:28 -0300

    feat/ mostrar itens ao concluir mini game

--- Assets/Scripts/Fase01/EndFase01.cs
+++ Assets/Scripts/Fase01/EndFase01.cs
@@ -6,9 +6,15 @@ public class EndFase01 : MonoBehaviour
 {
     [SerializeField]
     SceneInfo sceneInfo;
+    public GameObject itens;
+
 
     void Start()
     {
+        if (sceneInfo.destroyedObjects.Count > 0 && sceneInfo.destroyedObjects.Count < 3)
+        {
+            StartCoroutine(ShowItens());
+        }
         if (sceneInfo.destroyedObjects.Count == 3)
         {
             StartCoroutine(EndFase());
@@ -19,6 +25,15 @@ public class EndFase01 : MonoBehaviour
     {
         yield return new WaitForSeconds(0.1f);
         SceneManager.LoadScene("Fim");
+    }
+
+    IEnumerator ShowItens()
+    {
+        yield return new WaitForSeconds(0.1f);
+        itens.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        itens.SetActive(false);
+
     }
 }
 
